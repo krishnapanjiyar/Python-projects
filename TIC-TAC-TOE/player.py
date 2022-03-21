30b875d25b7f623f53f0a2adc292fa4c3d788340
krishnapanjiyar <krishna.panjiyar789@gmail.com>
2022-03-21 16:17:52 -0700

    game is half done

---
+++ TIC-TAC-TOE/player.py
@@ -0,0 +1,25 @@
+import math
+import random
+
+class Player:
+    def __init__(self, letter):
+        # letter is x or o
+        self.letter = letter
+        
+    # we want all players to get their next move given a game
+    def get_move(self, game):
+        pass
+
+class RandomComputerPlayer(Player):
+    def _init_(self, letter):
+        super()._init_(letter)
+
+    def get_move(self, game):
+        pass
+
+class HumanPlayer(Player):
+    def _init_(self, letter):
+        super()._init_(letter)
+
+    def get_move(self, game):
+        pass
