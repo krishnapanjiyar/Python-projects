b59d18e3b348dbebc0588cbd7a1213cab6591c1d
krishnapanjiyar <krishna.panjiyar789@gmail.com>
2022-03-20 17:21:03 -0700

    Computer guessing game done

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ def guess(x):
     guess = 0
     while guess != random_number:
         guess = int(input(f'Guess a number between 1 and {x}: '))
-        print(guess)
         if guess < random_number:
             print('Sorry, guess again. Too Low.')
         elif guess > random_number:
@@ -13,4 +12,22 @@ def guess(x):
 
     print(f'Yay, congrats. You have guessed the number {random_number} correctly!!')
 
-guess(10)   
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low #could also be high b/c low = high
+        guess = random.randint(low, high)
+        feedback = input(f'Is {guess} too High (H), too Low (L), or correct (C)?? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        if feedback  == 'l':
+            low = guess + 1
+
+    print(f'Yay! The computer guessed your number, {guess}, correclty!')
+
+computer_guess(10)   
