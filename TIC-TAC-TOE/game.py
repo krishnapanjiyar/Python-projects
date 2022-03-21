c31bcb197fb0abbf668289d5419050d2527b3a6e
krishnapanjiyar <krishna.panjiyar789@gmail.com>
2022-03-21 16:51:27 -0700

    in progress

--- TIC-TAC-TOE/game.py
+++ TIC-TAC-TOE/game.py
@@ -19,11 +19,20 @@ class TicTacToe:
 
     def available_moves(self):
         return [i for i, spot in enumerate(self.board) if spot == ' ']
-        # return []
-        # moves = []
-        # for (i, x) in enumerate(self.board):
-        #     # ['x', 'x', 'o'] --> [(0, 'x'), (1, 'x'), (2, 'o')]
-        #     if spot == ' ':
-        #         moves.append(i)
-        # return moves
+
+    def empty_squares(self):
+        return ' ' in self.board
+
+    def num_empty_sqaures(self):
+        return self.board.count(' ')
        
+def play(game, x_player, o_player, prin_game=True):
+    if print_game:
+        game.print_board_nums()
+
+    letter = 'X' # starting letter
+    # iterate while the game still has empty squares
+    # (we don't have to worry about winner because we'll just return that
+    #  which breaks the loop)
+    while game.empty_sqares():
+        pass
--- TIC-TAC-TOE/player.py
+++ TIC-TAC-TOE/player.py
@@ -15,11 +15,28 @@ class RandomComputerPlayer(Player):
         super()._init_(letter)
 
     def get_move(self, game):
-        pass
+        # get a random valid spot for our next move
+        square = random.choice(game.available_moves())
+        return square
 
 class HumanPlayer(Player):
     def _init_(self, letter):
         super()._init_(letter)
 
     def get_move(self, game):
-        pass
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-9):')
+            # we're going to check that this is a correct value by trying to cast
+            # it to an integer, and if it's not, then we say its invalid
+            # if that spot is not available on the board, we also say its invalid
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = true # if these are successful, then yay!
+            except ValueError:
+                print('Invalid square. try again. ')
+
+        return val
