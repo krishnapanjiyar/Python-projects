20f265ef0681b19cebd821c99327f2703c1fe5d3
krishnapanjiyar <krishna.panjiyar789@gmail.com>
2022-03-23 19:35:09 -0700

    Working on new python project for the binary search

---
+++ binary_search.py
@@ -0,0 +1,9 @@
+# Implementation of binary search algorithm!!
+
+# We will rprove that binary search is faster than naive search
+
+# naive search: scan entire list and ask if its equal to the target
+# if yes, return the index
+# if no, then return -1
+def naive_search(l, target):
+    for i in range()
