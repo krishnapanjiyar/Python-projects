35c6427ed6c9b0f9918811dfc4aff46c166c5530
krishnapanjiyar <krishna.panjiyar789@gmail.com>
2022-03-23 19:50:15 -0700

    comparison of binary and naive serach

--- binary_search.py
+++ binary_search.py
@@ -6,4 +6,39 @@
 # if yes, return the index
 # if no, then return -1
 def naive_search(l, target):
-    for i in range()
+    # exmaple l = [1, 3, 10, 12]
+    for i in range(len(l)):
+        if l[i] == target:
+            return i
+    return -1
+
+
+# binary search uses divide and conquer!
+# we will leverage the fact that our list is SORTED  
+def binary_search(l, target, low=None, high=None):
+    if low is None:
+        low = 0
+    if high is None:
+        high = len(l) - 1
+
+    if high < low:
+        return -1
+
+    # example l = [1, 3, 5, 10, 12] # should return 3
+    midpoint = (len(l)) // 2 # 2
+
+    if l[midpoint] == target:
+        return midpoint
+    elif target < l[midpoint]:
+        return binary_search(l, target, low, midpoint-1)
+    else:
+        # target > l[midpoint]
+        return binary_search(l, target, midpoint+1, high)
+
+if __name__=='__main__':
+    # l = [1, 3, 5, 10, 12]
+    # target = 10
+    # print(naive_search(l, target))
+    # print(binary_search(l, target))
+
+    length = 10000
