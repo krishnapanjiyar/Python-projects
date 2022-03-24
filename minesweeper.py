d32e50eebf78212c7807b4b094cb9f6689461c8a
krishnapanjiyar <krishna.panjiyar789@gmail.com>
2022-03-24 14:49:34 -0700

    Creating a game called MineSweeper

---
+++ minesweeper.py
@@ -0,0 +1,9 @@
+# play the game
+def play(dim_size=10, num_bombs=10):
+    # step 1: create the board and plant the bombs
+    # step 2: show the user the board and ask for where they want to dig
+    # step 3a: if location is a bomb, show game over message
+    # step 3b: if location is not a bomb, dig recursively until each square is at least
+    #       next to a bomb 
+    # step 4: repeat stoeps 2 and 3a/b until there are no more places to dig -> VICTORY!
+    pass
