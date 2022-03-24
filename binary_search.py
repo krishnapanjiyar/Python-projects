e053a6282f712605720565ce26c0e3b0b1052dc8
krishnapanjiyar <krishna.panjiyar789@gmail.com>
2022-03-23 20:13:53 -0700

    Binary search comparison with naive search is done

--- binary_search.py
+++ binary_search.py
@@ -1,3 +1,5 @@
+import random
+import time
 # Implementation of binary search algorithm!!
 
 # We will rprove that binary search is faster than naive search
@@ -25,7 +27,7 @@ def binary_search(l, target, low=None, high=None):
         return -1
 
     # example l = [1, 3, 5, 10, 12] # should return 3
-    midpoint = (len(l)) // 2 # 2
+    midpoint = (low + high) // 2 # 2
 
     if l[midpoint] == target:
         return midpoint
@@ -42,3 +44,19 @@ if __name__=='__main__':
     # print(binary_search(l, target))
 
     length = 10000
+    sorted_list = set()
+    while len(sorted_list) < length:
+        sorted_list.add(random.randint(-3*length, 3*length))
+    sorted_list = sorted(list(sorted_list))
+
+    start = time.time()
+    for target in sorted_list:
+        naive_search(sorted_list, target)
+    end = time.time()
+    print("Naive search time: ", (end - start)/length, "seconds")
+
+    start = time.time()
+    for target in sorted_list:
+        binary_search(sorted_list, target)
+    end = time.time()
+    print("Binary search time: ", (end - start)/length, "seconds")
