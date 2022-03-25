a54da19b4fda318c3cb373682bbcb9c61f0abc75
krishnapanjiyar <krishna.panjiyar789@gmail.com>
2022-03-25 16:53:59 -0700

    The graph representation part is done. Now, starting on compose

--- graph-composer/graph.py
+++ graph-composer/graph.py
@@ -7,6 +7,8 @@ class Vertex:
     def __init__(self, value):
         self.value = value
         self.adjacent = {}  # nodes that have an edge
+        self.neighbors = []
+        self.neighbors_weights = []
 
     def add_edge_to(self, vertex, weight=0):
         # this is adding an edge to the vertex we input with weight
@@ -15,3 +17,39 @@ class Vertex:
     def increment_edge(self, vertex):
         # this is incrementing the weight of the edge
         self.adjacent[vertex] = self.adjacent.get(vertex, 0) + 1
+
+    def get_probability_map(self):
+        for (vertex, weight) in self.adjacent.items():
+            self.neighbors.append(vertex)
+            self.neighbors_weights.append(weight)
+
+    def next_word(self):
+        # randomly select next word ***Based on weight!!!
+        return random.choices(self.neighbors, weights=self.neighbors_weights)
+
+# Now that we have our vertex representation, we put this together in a graph
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+
+    def get_vertex_values(self):
+        # what are the values of all the vertices?
+        # in other words, return all possible words
+        return set(self.vertices.kerys())
+
+    def add_vertex(self, value):
+        self.vertices[value] = Vertex(value)
+
+    def get_vertex(self, value):
+        # what if the value isn't in the graph?
+        if value not in self.vertices:
+            self.add_vertex(value)
+        return self.vertices(value)     # get the Vertex object
+
+    def get_next_word(self, current_vertex):
+        self.vertices[current_vertex.value].next_word()
+        
+    def generate_probability_mappings(self):
+        for vertex in self.vertices.values():
+            vertex.get_probability_map()
