66823f10d1745d9d3a22586cb95639f9653669b6
krishnapanjiyar <krishna.panjiyar789@gmail.com>
2022-03-24 17:49:56 -0700

    In progree

--- minesweeper.py
+++ minesweeper.py
@@ -1,3 +1,5 @@
+import random
+
 # lets create a board object to represent the minesweeper game
 # this is so that we can just say "create a new board object", or
 # "dig here", or "render this game for this object"
@@ -9,12 +11,39 @@ class Board:
 
         # let's create the board
         # helper function!
-        self.board = self.make_
+        self.board = self.make_new_board() # plant the bombs
 
         # Initialize a set to keep track of which locations we've uncovered
         # we'll save (row, col) tuples into this set
         self.dug = set()  # if we dig at 0, 0, then self.dug = {(0,0)}
 
+    def make_new_board(self):
+        # construct a new board based on the dim size and num bombs
+        # we should construct the list of lists here (or whatever representation you prefer,
+        # but since we have a 2-D board, list of lists is most natural)
+
+        # generate a new board
+        board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
+        # this creates an array like this:
+        # [[None, None, ...., None],
+        #   None, None, ...., None],
+        # [...                    ],
+        # [[None, None, ...., None]]
+        # we can see how this represents a board!
+
+        # plant the bombs
+        bombs_planted = 0
+        while bombs_planted < self.num_bombs:
+            loc = random.randint(0, self.dim_size**2 - 1) # return a random integer N such that a <= N <= b
+            row = loc // self.dim_size  # we want the number of times dim_size goes into loc to tell us 
+            col = loc % self.dim_size # we want the remainder to tell us what index in that row to loop
+
+            if board[row][col] == '*':
+                # this means we've actually planted a bomb there already so keep going
+                continue
+
+            board[row][col] = '*' # plant the bomb
+            bombs_planted += 1
 # play the game
 def play(dim_size=10, num_bombs=10):
     # step 1: create the board and plant the bombs
