7ec5c718c6c7bb2e9a2aff8a935eab7ddfa8c198
krishnapanjiyar <krishna.panjiyar789@gmail.com>
2022-03-24 17:04:05 -0700

    MineSweeper in progress

--- minesweeper.py
+++ minesweeper.py
@@ -1,3 +1,20 @@
+# lets create a board object to represent the minesweeper game
+# this is so that we can just say "create a new board object", or
+# "dig here", or "render this game for this object"
+class Board:
+    def __init__(self, dim_size, num_bombs):
+        # let's keep track of these parameters. they'll be helpful later
+        self.dim_size = dim_size
+        self.num_bombs = num_bombs
+
+        # let's create the board
+        # helper function!
+        self.board = self.make_
+
+        # Initialize a set to keep track of which locations we've uncovered
+        # we'll save (row, col) tuples into this set
+        self.dug = set()  # if we dig at 0, 0, then self.dug = {(0,0)}
+
 # play the game
 def play(dim_size=10, num_bombs=10):
     # step 1: create the board and plant the bombs
