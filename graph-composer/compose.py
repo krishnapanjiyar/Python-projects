26b14e41b32206661bdb129be0920a2a843856b8
krishnapanjiyar <krishna.panjiyar789@gmail.com>
2022-03-25 17:46:28 -0700

    Composer is done with fixed bugs

--- graph-composer/compose.py
+++ graph-composer/compose.py
@@ -1,47 +1,72 @@
+import os
+import re
 import string
+import random
 from graph import Graph, Vertex
 
-def get_words_from_text(text_path):
-    with open(text_path, 'r') as f:
-        text = f.read()
 
-        text = ' '.join(text.split()) # this is saying turn whitespaces into just spaces
-        text = text.lower() # make everything lowercase to compare stuff
-        # now we could be complex and deal with punctuations... but there are cases where
-        # you might add a period such as (Mr. Brightside), but that's not really
-        # punctuation... so we just remove all the punctuation
-        # hello! it's me. --> hello its me
+def get_words_from_text(text_path):
+    with open(text_path, 'rb') as file:
+        text = file.read().decode("utf-8")
+
+        # remove [verse 1: artist]
+        # include the following line if you are doing song lyrics
+        # text = re.sub(r'\[(.+)\]', ' ', text)
+
+        text = ' '.join(text.split())
+        text = text.lower()
         text = text.translate(str.maketrans('', '', string.punctuation))
 
-    words = text.split()    # split on spaces again
+    words = text.split()
+
+    words = words[:1000]
+
     return words
+
 
 def make_graph(words):
     g = Graph()
-
-    previous_word = None
-
+    prev_word = None
     # for each word
     for word in words:
-        # check that word is in the graph, and if not then add it
+        # check that word is in graph, and if not then add it
         word_vertex = g.get_vertex(word)
-    # if there was a previous word, then add an edge if it does not already exist
-    # in the graph, otherwise increment weight by 1
-    if previous_word:
-        previous_word.increment_egde(word_vertex)
 
-    # set our word to the previous word and iterate!
-    previous_word = word_vertex
+        # if there was a previous word, then add an edge if does not exist
+        # if exists, increment weight by 1
+        if prev_word:  # prev word should be a Vertex
+            # check if edge exists from previous word to current word
+            prev_word.increment_edge(word_vertex)
 
-    # now remember that we want to generate the probability mappings before composing
-    # this is a great place to do it before we return the graph object
+        prev_word = word_vertex
+
     g.generate_probability_mappings()
 
     return g
 
+
+def compose(g, words, length=50):
+    composition = []
+    word = g.get_vertex(random.choice(words))
+    for _ in range(length):
+        composition.append(word.value)
+        word = g.get_next_word(word)
+
+    return composition
+
+
 def main():
-    # step 1: get words from text
-    # step 2: make a graph using those words
-    # step 3: get the next word for x number of words(defined by user)
-    # step 4: show the user!
-    pass
+    words = get_words_from_text('texts/hp_sorcerer_stone.txt')
+
+    # for song in os.listdir('songs/{}'.format(artist)):
+    # if song == '.DS_Store':
+    #     continue
+    # words.extend(get_words_from_text('songs/{artist}/{song}'.format(artist=artist, song=song)))
+
+    g = make_graph(words)
+    composition = compose(g, words, 100)
+    print(' '.join(composition))
+
+
+if __name__ == '__main__':
+    main()
--- graph-composer/graph.py
+++ graph-composer/graph.py
@@ -1,55 +1,53 @@
-# This is our Markov Chain representation
 import random
 
-# Define the graph in terms of vertices
 
-class Vertex:
+class Vertex(object):
     def __init__(self, value):
         self.value = value
-        self.adjacent = {}  # nodes that have an edge
+        self.adjacent = {}  # nodes that it points to
         self.neighbors = []
         self.neighbors_weights = []
 
+    def __str__(self):
+        return self.value + ' '.join([node.value for node in self.adjacent.keys()])
+
     def add_edge_to(self, vertex, weight=0):
-        # this is adding an edge to the vertex we input with weight
         self.adjacent[vertex] = weight
 
     def increment_edge(self, vertex):
-        # this is incrementing the weight of the edge
         self.adjacent[vertex] = self.adjacent.get(vertex, 0) + 1
 
+    def get_adjacent_nodes(self):
+        return self.adjacent.keys()
+
+    # initializes probability map
     def get_probability_map(self):
         for (vertex, weight) in self.adjacent.items():
             self.neighbors.append(vertex)
             self.neighbors_weights.append(weight)
 
     def next_word(self):
-        # randomly select next word ***Based on weight!!!
-        return random.choices(self.neighbors, weights=self.neighbors_weights)
+        return random.choices(self.neighbors, weights=self.neighbors_weights)[0]
 
-# Now that we have our vertex representation, we put this together in a graph
 
-class Graph:
+class Graph(object):
     def __init__(self):
         self.vertices = {}
 
     def get_vertex_values(self):
-        # what are the values of all the vertices?
-        # in other words, return all possible words
-        return set(self.vertices.kerys())
+        return set(self.vertices.keys())
 
     def add_vertex(self, value):
         self.vertices[value] = Vertex(value)
 
     def get_vertex(self, value):
-        # what if the value isn't in the graph?
         if value not in self.vertices:
             self.add_vertex(value)
-        return self.vertices(value)     # get the Vertex object
+        return self.vertices[value]
 
     def get_next_word(self, current_vertex):
-        self.vertices[current_vertex.value].next_word()
-        
+        return self.vertices[current_vertex.value].next_word()
+
     def generate_probability_mappings(self):
         for vertex in self.vertices.values():
             vertex.get_probability_map()
